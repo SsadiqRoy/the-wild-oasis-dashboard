b2be42ff2e421030fa6c3ed85c24a30d5295e5f5
Ssadiq Roy <ssadiqueroy@gmail.com>
2026-09-03 13:51:54 +0000

    to dist

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  // build: { outDir: 'oasis-dash' },
+  build: { outDir: "dash" },
 });
