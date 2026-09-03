6eb6ae75129d87b5efde63d3c3c5158b00bb641a
Ssadiq Roy <73924009+SsadiqRoy@users.noreply.github.com>
2026-09-03 13:25:31 +0000

    Change Supabase URL and key
    Updated Supabase URL and key for the client.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://yswzkkjhmugpttpyeoqw.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzd3pra2pobXVncHR0cHllb3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4NjM1NzcsImV4cCI6MjAzMTQzOTU3N30.16_S6fRBYwR8q6OwUr_b4cPMl7d4SHns9ZEUdWHBmko';
+// export const supabaseUrl = 'https://yswzkkjhmugpttpyeoqw.supabase.co';
+// const supabaseKey =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzd3pra2pobXVncHR0cHllb3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4NjM1NzcsImV4cCI6MjAzMTQzOTU3N30.16_S6fRBYwR8q6OwUr_b4cPMl7d4SHns9ZEUdWHBmko';
+export const supabaseUrl = 'https://pmdibsulxbffbebbqmiw.supabase.co';
+const supabaseKey = 'sb_publishable_lAxlysXqgyB9uYLVot194A_qSEL2uhU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
